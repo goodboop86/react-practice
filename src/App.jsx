8636efd12480a1f565b7de7f0e166db6a1f549f4
goodboop86 <goodboop86@gmail.com>
2022-04-28 22:35:45 +0900

    done phase2

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
-import { useState } from 'react';
+import { useState, memo, useCallback } from 'react';
 import { Child1 } from "./components/Child1"
 import { Child4 } from "./components/Child4"
 import { TailwindCss } from './components/TailwindCss'
 
 
-export const App = () => {
+export const App = memo(() => {
   console.log("Appレンダリング");
   const [num, setNum] = useState(0);
   const onClickButton = () => {
     setNum(num + 1);
   }
+
+  const onClickReset = useCallback(() => {
+    setNum(0);
+  }, []);
+
   return (
     <>
       <button onClick={onClickButton}>ボタン</button>
       <p>{num}</p>
-      <Child1 />
+      {/* ↓Propsとして関数を設定*/}
+      <Child1 onClickReset={onClickReset} />
       <Child4 />
       <TailwindCss />
     </>
   );
-}
+})
