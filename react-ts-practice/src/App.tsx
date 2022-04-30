f56020ae69360c4ae4c9153c9ebcfd9d5ff4d761
goodboop86 <goodboop86@gmail.com>
2022-04-30 18:35:38 +0900

    phrase2 done

---
+++ react-ts-practice/src/App.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import { ListItem } from './components/ListItem';
+import axios from "axios";
+import './App.css';
+import type { User } from './types/user';
+
+
+export const App = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    axios.get<User[]>("https://example.com/users").then((res) => {
+      setUsers(res.data)
+    })
+  }, []);
+
+  return (
+    <div>
+      {users.map(user => (
+        <ListItem id={user.id} name={user.name} age={user.age} personalColor={user.personalColor} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ react-ts-practice/src/components/ListItem.tsx
@@ -0,0 +1,15 @@
+import type { FC } from "react";
+import type { User } from "../types/user";
+
+export const ListItem: FC<User> = (props: User) => {
+  const { id, name, age, personalColor } = props;
+  return (
+    <p style={{ color: personalColor }}>
+      {id}:{name}({age})
+    </p>
+  );
+};
+
+ListItem.defaultProps = {
+  personalColor: "grey"
+}
