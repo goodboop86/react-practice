7dd60b0b3c6a0443158db02dc08cb5aaec484767
goodboop86 <goodboop86@gmail.com>
2022-04-30 20:41:03 +0900

    add test (doing)

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,40 @@
-import { useContext, useState } from 'react';
-import { AdminFlagContext } from './components/providers/AdminFlagProvider';
-import { Card } from "./components/Card"
+import { useState } from 'react';
+import axios from "axios";
 
 
 export const App = () => {
-  console.log("Appレンダリング");
+  const [userList, setUserList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-  const { isAdmin, setIsAdmin } = useContext(AdminFlagContext);
 
+  const onClicFetchUser = () => {
+    setIsLoading(true);
+    setIsError(false);
 
-  const onClickSwitch = () => {
-    setIsAdmin(!isAdmin);
+    axios.get("https://example.com/users")
+      .then(result => {
+        const users = result.data.map(user => ({
+          id: user.id,
+          name: `${user.lastname} ${user.firstname}`,
+          age: user.age
+        }));
+
+        setUserList(users);
+      })
+      .catch(() => setIsError(true))
+      .finally(() => setIsLoading(false));
   };
 
   return (
     <div>
-      {/* ↓管理者フラグによって文字を出し分け*/}
-      {isAdmin ? <span>管理者です</span> : <span>管理者以外です</span>}
-      <button onClick={onClickSwitch}>切り替え</button>
-      <Card />
+      <button onClick={onClicFetchUser}>ユーザ取得</button>
+      {isError && <p style={{ color: "red" }}>エラー発生しました。</p>}
+      {isLoading ? (<p>データ取得中です</p>
+      ) : (userList.map(user => (
+        <p key={user.id}>{`${user.id}(${user.age}歳)`}</p>
+      ))
+      )}
     </div>
-  )
+  );
 };
